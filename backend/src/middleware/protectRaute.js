40ab5de8a46f6b2f0bcb91dd701401ee5f76da2c
Raunaksrivastava0 <raunaksri888@gmail.com>
2026-02-22 15:14:06 +0530

    minor typo fixes

--- backend/src/middleware/protectRaute.js
+++ backend/src/middleware/protectRaute.js
@@ -7,18 +7,18 @@ export const protectRoute = [
         try {
             const clerkId = req.auth().userId; // get the user ID from the auth object
             if(!clerkId){
-                return res.status(401).json({msg:"Unauthorized-invalid token"});
+                return res.status(401).json({message:"Unauthorized-invalid token"});
             }
             //find the user in the database using the clerkId
             const user = await User.findOne({clerkId}); // find the user in the database using the clerkId
             if(!user){
-                return res.status(404).json({msg:"Unauthorized-user not found"});
+                return res.status(404).json({message:"Unauthorized-user not found"});
             }
             req.user = user; // add the user object to the request for use in the next middleware or route handler
             next(); // call the next middleware or route handler
         } catch (error) {
             console.error("Error in protectRoute middleware:", error);
-            res.status(500).json({msg:"Server error"});
+            res.status(500).json({message:"Server error"});
         }
     },
 ];
